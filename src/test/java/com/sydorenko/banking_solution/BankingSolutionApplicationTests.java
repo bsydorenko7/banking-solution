3f4043f044af9dd7ff631c5d395bbd49424a0e07
bmbmbm9 <bmbmbm9@protonmail.com>
2024-10-06 19:14:10 +0300

    init commit

---
+++ src/test/java/com/sydorenko/banking_solution/BankingSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sydorenko.banking_solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
